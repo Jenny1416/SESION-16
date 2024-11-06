98b62f491a72a5bf603ad1a6f3b3109d45b811cd
Jenny1416 <jeniferp-lopeza@unilibre.edu.co>
2024-11-06 01:08:48 -0500

    ahorasi1

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById("contactButton").addEventListener("click", function() {
+document.getElementById("contactButton").addEventListener("click", function () {
     window.location.href = "index2.html";
 });
